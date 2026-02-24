1f721d5cc03eb9e7e0cf06ef6c876a71b1d0e3c2
John Glista <jwglista@gmail.com>
2026-02-23 19:38:26 -0500

    Add new OG image, remove edit page button on posts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://codemode.blog/",
   author: "John Glista",
   profile: "",
   desc: "A blog capturing my learnings on software development, AI, and other tech topics.",
   title: "John's Tech Blog",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "codemode-og.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
@@ -19,5 +19,5 @@ export const SITE = {
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "America/New_York", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
